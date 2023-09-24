78a33675696ee6c69fe59097b762e3fe434a1de8
Dmitry <kip@gmail.com>
2023-09-24 21:32:01 +0700

    dz

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,23 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees (
+	employee_id	int primary key,
+	first_name varchar(15) not null,
+	last_name varchar(15) not null,
+	title varchar(50) not null,
+	birth_date date,
+	notes text
+);
+
+CREATE TABLE customers(
+	customer_id varchar(10) primary key,
+	company_name varchar(50) not null,
+	contact_name varchar(50) not null
+);
+
+CREATE TABLE orders(
+	order_id int primary key,
+	customer_id varchar(5) not null,
+	employee_id int,
+	order_date date,
+	ship_city varchar (50) not null
+)
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,61 @@
-"""Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+
+conn = psycopg2.connect(host='localhost', database='north', user='postgres', password='65900')
+try:
+    with conn:
+        with conn.cursor() as cur:
+            with open('../homework-1/north_data/employees_data.csv', 'r', encoding='utf-8') as csvfile:
+                reader = csv.reader(csvfile)
+                headers = next(reader)
+                for row in reader:
+                    cur.execute(f'INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)', (row[0],
+                                                                                           row[1],
+                                                                                           row[2],
+                                                                                           row[3],
+                                                                                           row[4],
+                                                                                           row[5]))
+                cur.execute('SELECT * FROM employees')
+                data = cur.fetchall()
+                for x in data:
+                    print(x)
+finally:
+    conn.close()
+
+conn = psycopg2.connect(host='localhost', database='north', user='postgres', password='65900')
+try:
+    with conn:
+        with conn.cursor() as cur:
+            with open('../homework-1/north_data/customers_data.csv', 'r', encoding='utf-8') as csvfile:
+                reader = csv.reader(csvfile)
+                headers = next(reader)
+                for row in reader:
+                    cur.execute(f'INSERT INTO customers VALUES (%s, %s, %s)', (row[0],
+                                                                               row[1],
+                                                                               row[2]))
+                cur.execute('SELECT * FROM customers')
+                data = cur.fetchall()
+                for x in data:
+                    print(x)
+finally:
+    conn.close()
+
+conn = psycopg2.connect(host='localhost', database='north', user='postgres', password='65900')
+try:
+    with conn:
+        with conn.cursor() as cur:
+            with open('../homework-1/north_data/orders_data.csv', 'r', encoding='utf-8') as csvfile:
+                reader = csv.reader(csvfile)
+                headers = next(reader)
+                for row in reader:
+                    cur.execute(f'INSERT INTO orders VALUES (%s, %s, %s, %s, %s)', (row[0],
+                                                                                    row[1],
+                                                                                    row[2],
+                                                                                    row[3],
+                                                                                    row[4]))
+                cur.execute('SELECT * FROM orders')
+                data = cur.fetchall()
+                for x in data:
+                    print(x)
+finally:
+    conn.close()
