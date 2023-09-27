46c0ab9c5923a1620371b832e62f2260dd1a74a4
Dmitry <kip@gmail.com>
2023-09-27 18:47:54 +0700

    Переосмысление

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,17 +1,7 @@
-CREATE TABLE orders(
-	order_id int primary key,
-	customer_id varchar(5) not null,
-	employee_id int,
-	order_date date,
-	ship_city varchar (50) not null
-);
-
-
 CREATE TABLE customers(
-	customer_id varchar(10) not null,
+	customer_id varchar(10) primary key,
 	company_name varchar(50) not null,
-	contact_name varchar(50) not null,
-	city_arrival int references orders(order_id)
+	contact_name varchar(50) not null
 );
 
 CREATE TABLE employees (
@@ -21,4 +11,12 @@ CREATE TABLE employees (
 	title varchar(50) not null,
 	birth_date date,
 	notes text
+);
+
+CREATE TABLE orders(
+	order_id int primary key,
+	customer_id varchar(5) references customers(customer_id),
+	employee_id int references employees(employee_id),
+	order_date date,
+	ship_city varchar (50) not null
 )
--- homework-1/main.py
+++ homework-1/main.py
@@ -5,16 +5,14 @@ conn = psycopg2.connect(host='localhost', database='north', user='postgres', pas
 try:
     with conn:
         with conn.cursor() as cur:
-            with open('../homework-1/north_data/orders_data.csv', 'r', encoding='utf-8') as csvfile:
+            with open('../homework-1/north_data/customers_data.csv', 'r', encoding='utf-8') as csvfile:
                 reader = csv.reader(csvfile)
                 headers = next(reader)
                 for row in reader:
-                    cur.execute(f'INSERT INTO orders VALUES (%s, %s, %s, %s, %s)', (row[0],
-                                                                                    row[1],
-                                                                                    row[2],
-                                                                                    row[3],
-                                                                                    row[4]))
-                cur.execute('SELECT * FROM orders')
+                    cur.execute(f'INSERT INTO customers VALUES (%s, %s, %s)', (row[0],
+                                                                               row[1],
+                                                                               row[2]))
+                cur.execute('SELECT * FROM customers')
                 # data = cur.fetchall()
                 # for x in data:
                 # print(x)
@@ -46,24 +44,18 @@ conn = psycopg2.connect(host='localhost', database='north', user='postgres', pas
 try:
     with conn:
         with conn.cursor() as cur:
-            with open('../homework-1/north_data/customers_data.csv', 'r', encoding='utf-8') as csvfile:
-                with open('../homework-1/north_data/orders_data.csv', 'r', encoding='utf-8') as csvfile2:
-                    reader = csv.reader(csvfile)
-                    next(reader)
-                    for row in reader:
-                        csvfile2.seek(0)
-                        reader2 = csv.reader(csvfile2)
-                        next(reader2)
-                        for x in reader2:
-                            if row[0] in x:
-                                cur.execute(f'INSERT INTO customers VALUES (%s, %s, %s, %s)', (row[0],
-                                                                                               row[1],
-                                                                                               row[2],
-                                                                                               x[0]))
-
-                cur.execute('SELECT * FROM customers')
-                data = cur.fetchall()
-                for x in data:
-                    print(x)
+            with open('../homework-1/north_data/orders_data.csv', 'r', encoding='utf-8') as csvfile:
+                reader = csv.reader(csvfile)
+                headers = next(reader)
+                for row in reader:
+                    cur.execute(f'INSERT INTO orders VALUES (%s, %s, %s, %s, %s)', (row[0],
+                                                                                    row[1],
+                                                                                    row[2],
+                                                                                    row[3],
+                                                                                    row[4]))
+                cur.execute('SELECT * FROM orders')
+                # data = cur.fetchall()
+                # for x in data:
+                # print(x)
 finally:
     conn.close()
